b2bcb9206b7be26741af2d77c926b50fe691b74e
Shivani <ssp225@cornell.edu>
2018-02-21 15:15:21 -0500

    hi

---
+++ includes/navigation.php
@@ -0,0 +1,7 @@
+<nav>
+  <ul>
+    <li><a class="links" href="index.php">ABOUT</a></li>
+    <li><a class="links" href="resume.php">RESUME</a></li>
+    <li><a class="links" href="contact.php">CONTACT</a></li>
+  </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset = "UTF-8">
+  <title> Shivani Patel </title>
+  <link rel="stylesheet" type="text/css" href="style/all.css" media="all"/>
+</head>
+<body>
+  <?php
+  include("includes/navigation.php");
+  ?>
+  <h1> Welcome to the official website for Ithaca NY's Apple Festival! </h1>
+  <div class="homepage">
+    <figure>
+      <!-- Image source: http://cornellsun.com/2015/10/04/ithacans-flock-to-commons-for-applefest/ -->
+      <img class="welcome" alt="Apple Fest Commons for Home Page" src="images/home.jpg"/>
+      <figcaption class="source"> Source: http://cornellsun.com/2015/10/04/ithacans-flock-to-commons-for-applefest/ </figcaption>
+    </figure>
+    <div class="datediv">
+      <h2 id="comevisitmobile"> Come visit this yearly event that families have enjoyed since 1982. </h2>
+      <p class="date"> Friday September 29, 2017 <br/> 12pm - 6pm </p>
+      <p class="date"> Saturday September 30, 2017 <br/> 10am - 6pm </p>
+      <p class="date"> Sunday October 1, 2017 <br/> 10am - 6pm </p>
+    </div>
+  </div>
+
+</body>
+
+</html>
